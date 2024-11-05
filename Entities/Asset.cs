411389db26aeb6b893e67eb6e838167748456718
ComandantePaulus <46773362+ComandantePaulus@users.noreply.github.com>
2024-11-05 16:40:52 +0300

    Initial commit

---
+++ Entities/Asset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WT_Lab.Domain
+{
+    public class Asset
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int InvNumber { get; set; }
+        public decimal Price { get; set; }
+        public string? Photo { get; set; }
+
+        //Категория
+        public Category Category { get; set; }
+    }
+}
